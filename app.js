70f1ef7983a687876237d98eebc35d7cfcc6a775
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-05-10 02:07:16 +0200

    Created array and add the card options

---
+++ app.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Card options
+    const cardArray = [
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'blank',
+            img: 'images/blank.png'
+        },
+        {
+            name: 'white',
+            img: 'images/white.png'
+        },
+    ]
+
+    // Create game board
+});
